32f5d48f6ac6ffeb778d03f428cf2bca60c8fb82
JosephJvB <joevanbo@pm.me>
2019-08-04 22:07:21 +1200

    edit before publish

--- firefox/index.js
+++ firefox/index.js
@@ -1,7 +1,7 @@
-console.log('\n\n joe script loaded \n\n')
-// https://stackoverflow.com/questions/39993676/code-inside-domcontentloaded-event-not-working/39993735
+console.log('\n\n NZ-Herald unblocker script loaded \n\n');
 try
 {
+  // https://stackoverflow.com/questions/39993676/code-inside-domcontentloaded-event-not-working/39993735
   if (document.readyState !== 'loading')
   {
     const articleId = 'article-content';
@@ -21,23 +21,29 @@ try
 }
 catch (e)
 {
-  console.error('Joe caught an error\n', e);
+  console.error('Unblocker script error:\n', e);
 }
 
 /*
   elements hidden by
-  1. style {display: none}
-  2. class {display: none}
-  3. class {color: white}
-  4. class {opacity: 0}
-  5. class {opacity: 0}
+  style {display: none}
+  class {
+    display: none;
+    color: white;
+    opacity: 0;
+  }
+
+  set timeout is my superstitious "avoid redirect" tactic
 
   set interval is just to do a fancy reveal..
  */
-function revealElement (e, i) {
+function revealElement (e, i)
+{
+  e.style.opacity = 0;
+
   setTimeout(() => {
-    e.style.opacity = 0;
     e.style.color = 'black';
+    e.className = '';
     if(e.style.display === 'none')
     {
       e.style.removeProperty('display');
@@ -48,7 +54,7 @@ function revealElement (e, i) {
         e.style.opacity = (Number(e.style.opacity) + 0.1).toString();
       }
     }, 100);
-  }, 2000 * i);
+  }, 1300 * i);
 
   return;
 }
